368193fad265252cef6989327364214874f7961e
ducang <katorin123@gmail.com>
2019-05-12 16:37:23 +0700

    1

--- first.py
+++ first.py
@@ -1,11 +1,24 @@
-print("c4t18")
-print("asas")
-
+# print("c4t18")
+# print("asas")
 # var = 1
 # var1 = 1.1
 # string = "lul"
 # a = var - var1
 # print(a)
-number=int(input("dien 1 so:"))
-oh=int(input("dien 1 so:"))
-print(oh-number)
+# number=int(input("dien 1 so:"))
+# oh=int(input("dien 1 so:"))
+# total= number - oh
+# print(number,"-",oh,"=",total)
+# st_name=(input("first name:"))
+# last_name=(input("last name:"))
+# print(st_name+" "+last_name)
+# import math
+# r=int(input("ban kinh:"))
+# area=math.floor(r*r*math.pi)
+# print("area =", area)
+# perimeter= math.floor(r*2*math.pi)
+# print("perimeter =", perimeter)
+# year = 2019
+# bornin = int(input("nam sinh:"))
+# age = year - bornin
+# print("so tuoi la:"," ",age)

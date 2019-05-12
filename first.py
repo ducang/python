144f667b82a304981b33d90c1c4e0376e90e241c
ducang <katorin123@gmail.com>
2019-05-12 16:08:45 +0700

    1

---
+++ first.py
@@ -0,0 +1,11 @@
+print("c4t18")
+print("asas")
+
+# var = 1
+# var1 = 1.1
+# string = "lul"
+# a = var - var1
+# print(a)
+number=int(input("dien 1 so:"))
+oh=int(input("dien 1 so:"))
+print(oh-number)

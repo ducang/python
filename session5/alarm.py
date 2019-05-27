5bfce354cc897d8d4a6204d3e0d58b1fea454e2d
ducang <katorin123@gmail.com>
2019-05-27 19:28:36 +0700

    -

--- session5/alarm.py
+++ session5/alarm.py
@@ -1,3 +1,14 @@
 import pyglet
-pyglet.lib.load_library('avbin')
-pyglet.have_avbin=True
+import time
+import datetime
+
+
+now= str(datetime.datetime.today())
+while (now>=int(19:25:00.000000)):
+    print (now)
+    music=pyglet.resource.media("music.mp3")
+    music.play()
+    pyglet.app.run()
+    
+
+    

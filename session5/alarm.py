d34a32fce50de9d1e913bb34720d6783755c31c7
ducang <katorin123@gmail.com>
2019-05-27 20:08:25 +0700

    ok

--- session5/alarm.py
+++ session5/alarm.py
@@ -1,14 +1,14 @@
 import pyglet
 import time
 import datetime
-
-
-now= str(datetime.datetime.today())
-while (now>=int(19:25:00.000000)):
-    print (now)
-    music=pyglet.resource.media("music.mp3")
-    music.play()
-    pyglet.app.run()
+now= str(datetime.datetime.today().time())
+alarm = "19:59:00.000000"
+while (now>=alarm):
+  music=pyglet.resource.media("music.mp3")
+  music.play()
+  pyglet.app.run()  
     
+
+        
 
     

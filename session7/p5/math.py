d126652f0b5a590addc96447056a1753225de8e0
ducang <katorin123@gmail.com>
2019-06-06 17:51:29 +0700

    latest

--- session7/p5/math.py
+++ session7/p5/math.py
@@ -4,7 +4,10 @@ while True:
     a = random.randint(-10,10)
     b = random.randint(-10,10)
     sum = random.randint(-20,20)
-    print (a ,'+',b,'=', sum)
+    if (b >= 0):
+        print (a ,'+',b,'=', sum)
+    else:
+        print (a,b,'=',sum)  
     ans = input("True or False:")
     if sum == a+b:
         if ans == 'true':

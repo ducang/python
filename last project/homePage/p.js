128a6db6a200dc999e7da188f35ab4cac836a1e3
ducang <katorin123@gmail.com>
2019-11-03 14:25:39 +0700

    'new;

--- last project/homePage/p.js
+++ last project/homePage/p.js
@@ -1,6 +1,22 @@
-function home() {
-    window.location='file:///C:/Users/omg37/Documents/C4T18/last%20project/p.html';
+var slideIndex = 0;
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    console.log(slideIndex)
+    console.log(slides[slideIndex - 1])
+    slides[slideIndex - 1]["style"]["display"] = "block";
+    dots[slideIndex - 1].className += " active";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
 }
-function python() {
-    window.location='file:///C:/Users/omg37/Documents/C4T18/last%20project/code/python.html';
-}
+
+showSlides();
